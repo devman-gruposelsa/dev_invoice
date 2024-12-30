28f394320b4da02050e567574da4181952bd3533
MatiasBanega <banegamatias@gmail.com>
2024-12-30 11:59:28 -0300

    Commit inicial

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Dev_invoice',
+    'version': '',
+    'summary': """ Dev_invoice Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    'data': [
+        
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
