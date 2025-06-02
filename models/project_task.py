67878a96e1884e484f9b79b9e1804ac393be9fe5
MatiasBanega <banegamatias@gmail.com>
2025-06-01 21:02:07 -0300

    UPD

--- models/project_task.py
+++ models/project_task.py
@@ -825,7 +825,7 @@ class ProjectTask(models.Model):
             return True
 
         action_vals = {
-            'name': self.env._('Generated Monthly Invoices'),
+            'name': 'Generated Monthly Invoices',
             'domain': [('id', 'in', created_invoice_ids)],
             'res_model': 'account.move',
             'type': 'ir.actions.act_window',
